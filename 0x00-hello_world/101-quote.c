b07a44d0dc6c382d89fb8140de3e89cfcb20c5aa
NafisaKaruri <nafeesahassan016@gmail.com>
2023-09-07 18:11:47 -0700

    edited 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,14 @@
 /**
  * main - Entry point
  *
- * Description: Print text followed by new line
+ * Description: Print text using write function
  *
  * Return: 1
  */
 int main(void)
 {
-	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char quot[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(1, quote, 59);
+	write(1, quot, 59);
 	return (1);
 }
