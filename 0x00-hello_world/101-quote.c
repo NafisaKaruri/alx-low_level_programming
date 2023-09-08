9fce14e03af078778abc55f88d27ebb9ae387ef0
NafisaKaruri <nafeesahassan016@gmail.com>
2023-09-07 18:06:38 -0700

    added 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Description: Print text followed by new line
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	write(1, quote, 59);
+	return (1);
+}
