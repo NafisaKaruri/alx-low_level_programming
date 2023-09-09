e96dfa12c7487743c8d915c257df965080d8e134
NafisaKaruri <nafeesahassan016@gmail.com>
2023-09-08 18:15:39 -0700

    edited 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,20 +14,18 @@ int main(void)
 
 	for (a = 0; a <= 99; a++)
 	{
-		for (b = a; b <= 99; b++)
+		for (b = a + 1; b <= 99; b++)
 		{
-			if (a != b)
+
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+			if (a != 98 || b != 99)
 			{
-				putchar((a / 10) + '0');
-				putchar((a % 10) + '0');
+				putchar(',');
 				putchar(' ');
-				putchar((b / 10) + '0');
-				putchar((b % 10) + '0');
-				if (a != 98 || b != 99)
-				{
-					putchar(',');
-					putchar(' ');
-				}
 			}
 		}
 	}
