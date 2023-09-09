753dec5a675149377c8427792e7b8858b99adad7
NafisaKaruri <nafeesahassan016@gmail.com>
2023-09-08 18:12:03 -0700

    edited 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,6 +11,7 @@
 int main(void)
 {
 	int a, b;
+
 	for (a = 0; a <= 99; a++)
 	{
 		for (b = a; b <= 99; b++)
@@ -22,7 +23,7 @@ int main(void)
 				putchar(' ');
 				putchar((b / 10) + '0');
 				putchar((b % 10) + '0');
-				if (a != 98 || b !=99)
+				if (a != 98 || b != 99)
 				{
 					putchar(',');
 					putchar(' ');
