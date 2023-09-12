4e419c57d1bccbb122d7f19dd22d66f4d6483fc4
NafisaKaruri <nafeesahassan016@gmail.com>
2023-09-12 11:08:15 -0700

    added 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: t prints 10 times alphabet, in lowercase,
+ *		using prototype print_alphabet_x10
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char j;
+
+	while (i <= 9)
+	{
+		j = 'a';
+		while (j <= 'z')
+		{
+			_putchar(j);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
