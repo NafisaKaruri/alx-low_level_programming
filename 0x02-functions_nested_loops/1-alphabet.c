5608a908e08565dd94712796f488c8a4d14f9b76
NafisaKaruri <nafeesahassan016@gmail.com>
2023-09-12 10:49:44 -0700

    added 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main -Entry point
+ *
+ * Description: prints alphabets in lowercase using prototype
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
