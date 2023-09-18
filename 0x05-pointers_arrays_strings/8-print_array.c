9661060f311fd5185d1932fa364b9f74ebfd9aef
NafisaKaruri <nafeesahassan016@gmail.com>
2023-09-18 13:52:59 -0700

    added 8-print_array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,15 +11,12 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if (i = n - 1)
+		if (i == n - 1)
 			printf("%d\n", a[i]);
-		else
+		while (i < n - 1)
 		{
-			while (i < n - 1)
-			{
-				printf("%d, ", a[i]);
-				i++;
-			}
+			printf("%d, ", a[i]);
+			i++;
 		}
 	}
 }
