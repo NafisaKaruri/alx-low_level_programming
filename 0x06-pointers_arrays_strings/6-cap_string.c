0d88f9da3a743027c4147c64f0e14bffcd5bcb4e
NafisaKaruri <nafeesahassan016@gmail.com>
2023-09-21 14:14:14 -0700

    edited 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <ctype.h>
-
+#include <string.h>
 /**
  * cap_string - capitalizes all words of a string
  * @s: the string to be edited
@@ -10,11 +10,11 @@
 char *cap_string(char *s)
 {
 	int i;
-	/*char sep[] = " \t\n,;.!?"(){}";*/
+	char separators[] = " \t\n,;.!?'\"(){}";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' || s[i] == ',' || s[i] == ';' || s[i] == '.' || s[i] == '!' || s[i] == '?' || s[i] == '"' || s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}')
+		if (i == 0 || strchr(separators, s[i]) != NULL)
 		{
 			if (isalpha(s[i + 1]) && islower(s[i + 1]))
 				s[i + 1] = (int)s[i + 1] - 32;
