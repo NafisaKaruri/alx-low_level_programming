e7b40a3b80e129a495d426876b582390fa882563
NafisaKaruri <nafeesahassan016@gmail.com>
2023-10-02 16:58:52 -0700

    edit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * main - adds positive numbers
@@ -21,6 +22,11 @@ int main(int argc __attribute__((unused)), char **argv __attribute__((unused)))
 	{
 		while (i < argc)
 		{
+			if (strstr(argv[i], "$") == NULL)
+			{
+				printf("Error\n");
+				return (1);
+			}
 			if (atoi(argv[i]) == 0 && *argv[i] != '0')
 			{
 				printf("Error\n");
