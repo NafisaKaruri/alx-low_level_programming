141fed467f01b8f532995c6bf185b1fdf6bd24d1
NafisaKaruri <nafeesahassan016@gmail.com>
2023-10-02 14:31:19 -0700

    edit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,7 +12,7 @@ int main(int argc __attribute__((unused)), char **argv __attribute__((unused)))
 {
 	int product = 1, i = 1;
 
-	if (argc <= 1)
+	if (argc <= 2)
 	{
 		printf("Error\n");
 		return (1);
