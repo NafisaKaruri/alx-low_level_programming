1a4d2c421efaec9127957e82676e1ce9d221307d
NafisaKaruri <nafeesahassan016@gmail.com>
2023-10-07 13:15:15 -0700

    added 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -26,8 +26,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 	if (ptr == NULL)
-		return malloc(new_size);
-
+		return (malloc(new_size));
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
 	p = malloc(new_size);
 	if (p == NULL)
 		return (NULL);
