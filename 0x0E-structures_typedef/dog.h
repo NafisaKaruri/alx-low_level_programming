1dc0fc819267d350d693e340403c736b2e3f0e46
NafisaKaruri <nafeesahassan016@gmail.com>
2023-10-10 14:54:12 -0700

    added dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog information
+ * @name: the dog name
+ * @age: the dog age
+ * @owner: the dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
