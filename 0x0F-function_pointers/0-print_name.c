54c3ed40ab6e07c84c2159577e3b44b0052c6a9f
NafisaKaruri <nafeesahassan016@gmail.com>
2023-10-11 11:15:09 -0700

    added 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name of the person
+ * @f: the function to show the style of the printing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
