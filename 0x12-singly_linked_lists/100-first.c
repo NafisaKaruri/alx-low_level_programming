14c0659b1edbf9e396ff264096ca6254dae3a3a2
NafisaKaruri <nafeesahassan016@gmail.com>
2023-10-19 13:37:18 -0700

    added 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * first - prints a string before the main is execute
+ */
+void __attribute__((constructor)) first()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
