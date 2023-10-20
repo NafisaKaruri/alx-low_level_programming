59f4a56573e8f9eec8782d194a758d65d331bcde
NafisaKaruri <nafeesahassan016@gmail.com>
2023-10-20 10:10:10 -0700

    updated

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,13 @@
 section .data
-	hello db "Hello, Holberton", 10, 0
+        hello db "Hello, Holberton", 10, 0
 
 section .text
-	global main
-	extern printf
+        global main
+        extern printf
 
 main:
-	mov rdi, hello ; the string to print
-	xor al, al     ; number of vectors used for argumuments	
-	call printf    ; call printf
-	mov eax, 60    ; system call number for exit
-	xor edi, edi   ; exit code
-	syscall        ; make system call
+        mov rdi, hello
+        call printf
+        mov rax, 60
+        xor rdi, rdi
+        syscall
