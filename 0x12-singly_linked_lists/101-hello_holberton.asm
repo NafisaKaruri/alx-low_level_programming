8e19791ea935db7b7856590fdbd4a6fe77b7fb27
NafisaKaruri <nafeesahassan016@gmail.com>
2023-10-20 10:18:23 -0700

    updated

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,14 @@
 section .data
-        hello db "Hello, Holberton", 10, 0
-
+	format db "Hello, Holberton", 10, 0
 section .text
         global main
         extern printf
 
 main:
-        mov rdi, hello
+	push rbp
+        mov rdi, format
         call printf
+	add rsp, 8
         mov rax, 60
         xor rdi, rdi
         syscall
