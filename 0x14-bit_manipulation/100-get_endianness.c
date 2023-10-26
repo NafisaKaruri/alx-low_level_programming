e411da2667b7babe95e502d0172d2cd73aac461e
NafisaKaruri <nafeesahassan016@gmail.com>
2023-10-26 15:11:08 -0700

    added 100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	int x = 1;
+	char *p = (char *)&x;
+
+	if (*p == 0X01)
+		return (1);
+	return (0);
+}
