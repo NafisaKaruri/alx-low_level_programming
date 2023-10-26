d97aee77ea1024c468044a958c8a604e5c6ffdc3
NafisaKaruri <nafeesahassan016@gmail.com>
2023-10-26 12:50:38 -0700

    added 1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: the number
+ */
+void print_binary(unsigned long int n)
+{
+	int end;
+
+	if (n > 1)
+		print_binary (n >> 1);
+	end = n & 1;
+	_putchar (end + '0');
+}
