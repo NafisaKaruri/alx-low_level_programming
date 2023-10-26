a51997a77ea86cbcec56d811a8ffc9deca08809d
NafisaKaruri <nafeesahassan016@gmail.com>
2023-10-26 14:18:35 -0700

    added 2, 3, 4

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,7 +2,8 @@
 
 /**
  * get_bit - returns the value of a bit at a given index
- * @n: the index, starting from 0 of the bit you want to get
+ * @n: the number
+ * @index: the index, starting from 0 of the bit you want to get
  *
  * Return: the value of the bit at index index or -1 if an error occured
  */
