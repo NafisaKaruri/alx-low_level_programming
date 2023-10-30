1f7ee19e1766776ccb88d69c146e3806ac812672
NafisaKaruri <nafeesahassan016@gmail.com>
2023-10-30 13:19:30 -0700

    added 3-cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,71 @@
+#include "main.h"
+
+/**
+ * read_err - prints the error text and exits
+ * argv: argument vector
+ */
+void read_err(char **argv)
+{
+	dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+	exit(98);
+}
+
+/**
+ * write_err - prints the error text and exits
+ * argv: argument vector
+ */
+void write_err(char **argv)
+{
+	dprintf(2, "Error: Can't write to %s\n", argv[2]);
+	exit(99);
+}
+
+/**
+ * main - copies the content of a file to another file
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: 0 if success
+ */
+int main(int argc, char **argv)
+{
+	int fd, fd1, r, wcount, c;
+	char buffer[1024];
+
+	if (argc != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+		read_err(argv);
+
+	fd1 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd1 == -1)
+		write_err(argv);
+
+	while ((r = read(fd, buffer, 1024)) > 0)
+	{
+		wcount = write(fd1, buffer, r);
+		if (wcount != r)
+			write_err(argv);
+	}
+	if (r == -1)
+		read_err(argv);
+
+	c = close(fd);
+	if (c == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+	c = close(fd1);
+	if (c == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd1);
+		exit(100);
+	}
+	return (0);
+}
