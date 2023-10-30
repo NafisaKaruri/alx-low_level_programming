b41256c29a67e8363550517becae9304b01ee54c
NafisaKaruri <nafeesahassan016@gmail.com>
2023-10-30 14:08:48 -0700

    updated

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -38,9 +38,10 @@ int main(int argc, char **argv)
 		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
+	if (!argv[1])
+		read_err(argv);
 	fd = open(argv[1], O_RDONLY);
-	if (!argv[1] || fd == -1)
+	if (fd == -1)
 		read_err(argv);
 
 	fd1 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
@@ -55,7 +56,6 @@ int main(int argc, char **argv)
 	}
 	if (r == -1)
 		read_err(argv);
-
 	c = close(fd);
 	if (c == -1)
 	{
