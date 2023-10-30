5174cf7976d8a022c7468fd7ec5b7481837e9244
NafisaKaruri <nafeesahassan016@gmail.com>
2023-10-30 15:35:21 -0700

    updated

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,8 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <elf.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
