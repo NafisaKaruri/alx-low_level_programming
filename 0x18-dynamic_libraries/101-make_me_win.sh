b0a7fc106e748c4babbfd56c34d47b2a9492bc39
NafisaKaruri <nafeesahassan016@gmail.com>
2023-12-11 16:52:16 -0800

    :/

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P /tmp https://github.com/NafisaKaruri/alx-low_level_programming/raw/master/0x18-dynamic_libraries/librandom.so
-export LD_PRELOAD=/tmp/librand.so
+export LD_PRELOAD=/tmp/librandom.so
