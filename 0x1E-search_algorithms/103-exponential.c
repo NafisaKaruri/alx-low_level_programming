c2262a8ffbf10f0532e02ba21d5442d0d0a09a7c
NafisaKaruri <nafeesahassan016@gmail.com>
2024-04-14 09:19:58 -0700

    task 103

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -3,8 +3,9 @@
 /**
  * binary_searchs - searches for a value in an array using binary search
  *
- * @array: pointer to the first element of the aray to search in
- * @size: the number of elements in array
+ * @array: pointer to the first element of the array to search in
+ * @l: the start of the array to search
+ * @r: the end of the array to search
  * @value: the value to search for
  *
  * Return: -1 if value not present or array is NULL. Else
@@ -12,39 +13,47 @@
  */
 int binary_searchs(int *array, size_t l, size_t r, int value)
 {
-        size_t i;
+	size_t i;
 
-        /* If the array is NULL */
-        if (!array)
-                return (-1);
+	/* If the array is NULL */
+	if (!array)
+		return (-1);
 
-        for (; l <= r;)
-        {
-                /* Print the part being searched */
-                printf("Searching in array: ");
-                for (i = l; i < r; i++)
-                        printf("%d, ", array[i]);
-                printf("%d\n", array[i]);
+	for (; l <= r;)
+	{
+		/* Print the part being searched */
+		printf("Searching in array: ");
+		for (i = l; i < r; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
 
-                /* Find the middle of the array */
-                i = l + (r - l) / 2;
-                /* Value found in middle */
-                if (array[i] == value)
-                        return (i);
-                /* If value is less than middle, move the right boundary to middle */
-                if (array[i] > value)
-                        r = i - 1;
+		/* Find the middle of the array */
+		i = l + (r - l) / 2;
+		/* Value found in middle */
+		if (array[i] == value)
+			return (i);
+		/* If value is less than middle, move the right boundary to middle */
+		if (array[i] > value)
+			r = i - 1;
 
-                /* Else, move the left boundary to middle */
-                else
-                        l = i + 1;
-        }
+		/* Else, move the left boundary to middle */
+		else
+			l = i + 1;
+}
 
-        /* Value not found */
-        return (-1);
+/* Value not found */
+return (-1);
 }
 
 /**
+ * exponential_search - searches for a value in an array using exponential
+ *
+ * @array: pointer to the first element of the array to search in
+ * @size: the number of elements in array
+ * @value: the value to search for
+ *
+ * Return: -1 if value not present or array is NULL. Else
+ *         return the first index where value is located.
  */
 int exponential_search(int *array, size_t size, int value)
 {
