7b16ba14c63822b44912018c5b8e58312d39182e
NafisaKaruri <nafeesahassan016@gmail.com>
2024-04-14 06:36:46 -0700

    task 100

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,48 @@
+#include "search_algos.h"
+
+/**
+ * jump_search - searches for a value in a sorted array using jump search algorithm
+ *
+ * @array: pinter to the first element of the array to search in
+ * @size: the number of elements in array
+ * @value: the value to search for
+ *
+ * Return: -1 if value is not present in array or if array is NULL. Else
+ *	   return the first index where value is located.
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	size_t i, jump, step;
+
+	/* If the array is NULL or size <= 0 */
+	if (!array || size <= 0)
+		return (-1);
+
+	/* Let the step be the square root of the array size */
+	step = sqrt(size);
+
+	/* Loop with step size till value at the jump index >= value */
+	for (i = jump = 0; jump < size && array[jump] < value;)
+	{
+		printf("Value checked array[%ld] = [%d]\n", jump, array[jump]);
+
+		/* jump forward by step size */
+		i = jump;
+		jump += step;
+	}
+
+	printf("Value found between indexes [%ld] and [%ld]\n", i, jump);
+
+	/* if jump is greater than the size set it to last index */
+	jump = jump < size - 1 ? jump : size - 1;
+	/* Loop from the last jump index to current */
+	for (; i <= jump && array[i] <= value; i++)
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+
+	/* value found */
+	if (array[--i] == value)
+		return ((int)i);
+	/* value not found */
+	else
+		return (-1);
+}
--- 0x1E-search_algorithms/README.md
+++ 0x1E-search_algorithms/README.md
@@ -2,5 +2,9 @@
 
 In this project you will find the implementation of different search algorithms.
 
-0- linear
-1- binary
+0- linear search
+1- binary search
+2- jump search
+3- interpolation search
+4- exponential search
+5- advanced binary search
