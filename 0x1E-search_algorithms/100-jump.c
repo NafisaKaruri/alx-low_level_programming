64b7c0b0381b58bce01fdf357870831e2ca5121a
NafisaKaruri <nafeesahassan016@gmail.com>
2024-04-14 07:02:04 -0700

    double

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -37,10 +37,14 @@ int jump_search(int *array, size_t size, int value)
 	jump = jump < size - 1 ? jump : size - 1;
 	/* Loop from the last jump index to current */
 	for (; i <= jump && array[i] <= value; i++)
+	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			break;
+	}
 
 	/* value found */
-	if (array[--i] == value)
+	if (array[i] == value)
 		return (i);
 	/* value not found */
 	else
